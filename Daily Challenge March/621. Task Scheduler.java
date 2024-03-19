aa039385fe2d751887469e2335a8c7198d46bb62
Anup Bamrel <122713481+Anupmqr@users.noreply.github.com>
2024-03-19 22:37:41 +0530

    Create 621. Task Scheduler.java

---
+++ Daily Challenge March/621. Task Scheduler.java
@@ -0,0 +1,24 @@
+class Solution {
+    public int leastInterval(char[] tasks, int n) {
+        int[] arr = new int[26];
+
+        for(char x : tasks){
+            arr[x-'A']++;
+        }
+
+        int m=0;
+        for(int y : arr){
+            m=Math.max(m,y);
+        }
+
+        int n_m=0;
+        for(int y : arr){
+            if(m==y)
+                n_m++;
+        }
+
+        int ans = (m-1) * (n+1) + n_m;
+
+        return Math.max(ans,tasks.length);
+    }
+}
